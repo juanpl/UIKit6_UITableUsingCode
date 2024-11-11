edf2f858f04a5458e6694ead6a217a789721f7a2
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-11 11:22:04 -0500

    Initial Commit

---
+++ UIKit6_UITableUsingCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit6_UITableUsingCode
+//
+//  Created by Juan Pablo Lasprilla Correa on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
